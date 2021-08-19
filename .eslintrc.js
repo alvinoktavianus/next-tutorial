bf7d65f4b38a5f104c420ad462054ae7a63ef5ea
Alvin Oktavianus <agentsam1995@gmail.com>
2021-08-19 17:33:36 +0700

    add core dependencies to the project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/prop-types': 0,
+    'linebreak-style': ['error', 'unix'],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
